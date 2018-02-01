cc7eff989578e9a6ebd91fa665d058ced748ead8
lkatkus <laimonas@katkus.eu>
2018-02-01 17:07:42 +0200

    WIP

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ function init(){
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
-    // SETTING STARTING CANVAS SIZE
-    canvas.width = container.offsetWidth;
-    canvas.height = container.offsetHeight;
-
-
-    var block; /* BASIC BLOCK FOR FORM BUILDING */
+    // BLOCK PARAMETERS FOR BUILDING SHAPES
+    var block;
     var blockSize = 20;
 
+    // SETTING STARTING CANVAS SIZE
+    canvas.width = blockSize*12;
+    canvas.height = blockSize*20;
 
     function drawBlock(){
         console.log('drawBasic');
@@ -19,67 +18,155 @@ function init(){
         block = ctx.getImageData(0, 0, blockSize, blockSize);
         void ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-
     drawBlock();
 
     var makeShape = {
         leftGun: function(x,y){
-            console.log('leftGun');
-            ctx.putImageData(block,x,y);
-            ctx.putImageData(block,x+blockSize,y);
-            ctx.putImageData(block,x+blockSize*2,y);
-            ctx.putImageData(block,x+blockSize*2,y+blockSize);
+                console.log('leftGun');
+                this.x = x;
+                this.y = y;
+                ctx.putImageData(block,this.x,this.y);
+                ctx.putImageData(block,this.x+blockSize,this.y);
+                ctx.putImageData(block,this.x+blockSize*2,this.y);
+                ctx.putImageData(block,this.x+blockSize*2,this.y+blockSize);
         },
         rightGun: function(x,y){
             console.log('rightGun');
-            ctx.putImageData(block,x,y);
-            ctx.putImageData(block,x+blockSize,y);
-            ctx.putImageData(block,x+blockSize*2,y);
-            ctx.putImageData(block,x,y+blockSize);
+            this.x = x;
+            this.y = y;
+            ctx.putImageData(block,this.x,this.y);
+            ctx.putImageData(block,this.x+blockSize,this.y);
+            ctx.putImageData(block,this.x+blockSize*2,this.y);
+            ctx.putImageData(block,this.x,this.y+blockSize);
         },
         stick: function(x,y){
             console.log('stick');
-            ctx.putImageData(block,x,y);
-            ctx.putImageData(block,x+blockSize,y);
-            ctx.putImageData(block,x+blockSize*2,y);
-            ctx.putImageData(block,x+blockSize*3,y);
+            this.x = x;
+            this.y = y;
+            ctx.putImageData(block,this.x,this.y);
+            ctx.putImageData(block,this.x+blockSize,this.y);
+            ctx.putImageData(block,this.x+blockSize*2,this.y);
+            ctx.putImageData(block,this.x+blockSize*3,this.y);
         },
         box: function(x,y){
             console.log('box');
-            ctx.putImageData(block,x,y);
-            ctx.putImageData(block,x+blockSize,y);
-            ctx.putImageData(block,x,y+blockSize);
-            ctx.putImageData(block,x+blockSize,y+blockSize);
+            this.x = x;
+            this.y = y;
+            ctx.putImageData(block,this.x,this.y);
+            ctx.putImageData(block,this.x+blockSize,this.y);
+            ctx.putImageData(block,this.x,this.y+blockSize);
+            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
         },
         pyramid: function(x,y){
             console.log('pyramid');
-            ctx.putImageData(block,x,y);
-            ctx.putImageData(block,x+blockSize,y);
-            ctx.putImageData(block,x+blockSize,y+blockSize);
-            ctx.putImageData(block,x+blockSize*2,y);
+            this.x = x;
+            this.y = y;
+            ctx.putImageData(block,this.x,this.y);
+            ctx.putImageData(block,this.x+blockSize,this.y);
+            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
+            ctx.putImageData(block,this.x+blockSize*2,this.y);
         },
         rightKink: function(x,y){
             console.log('rightKink');
-            ctx.putImageData(block,x,y+blockSize);
-            ctx.putImageData(block,x+blockSize,y+blockSize);
-            ctx.putImageData(block,x+blockSize,y);
-            ctx.putImageData(block,x+blockSize*2,y);
+            this.x = x;
+            this.y = y;
+            ctx.putImageData(block,this.x,this.y+blockSize);
+            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
+            ctx.putImageData(block,this.x+blockSize,this.y);
+            ctx.putImageData(block,this.x+blockSize*2,this.y);
         },
         leftKink: function(x,y){
             console.log('leftKink');
-            ctx.putImageData(block,x,y);
-            ctx.putImageData(block,x+blockSize,y);
-            ctx.putImageData(block,x+blockSize,y+blockSize);
-            ctx.putImageData(block,x+blockSize*2,y+blockSize);
+            this.x = x;
+            this.y = y;
+            ctx.putImageData(block,this.x,this.y);
+            ctx.putImageData(block,this.x+blockSize,this.y);
+            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
+            ctx.putImageData(block,this.x+blockSize*2,this.y+blockSize);
         },
     };
 
-    makeShape.leftGun(100,0);
-    makeShape.rightGun(100,50);
-    makeShape.stick(100,100);
-    makeShape.box(100,150);
-    makeShape.pyramid(100,200);
-    makeShape.rightKink(100,250);
-    makeShape.leftKink(100,300);
+    function leftGun(x,y){
+        this.x = x;
+        this.y = y;
+
+        this.draw = function (){
+            // console.log('leftGun');
+
+            // activeShape.push(ctx.putImageData(block,this.x,this.y);
+            ctx.putImageData(block,this.x,this.y);
+            // ctx.putImageData(block,this.x+blockSize,this.y);
+            // ctx.putImageData(block,this.x+blockSize*2,this.y);
+            // ctx.putImageData(block,this.x+blockSize*2,this.y+blockSize);
+        },
+
+        this.update = function(){
+            taken = {};
+            let free = true;
+
+            void ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            for(let i = 0,j = finishedShapes.length; i < j; i++){
+                if(this.y == finishedShapes[i].y){
+                    console.log('taken');
+                    free = false;
+                }else{
+                    console.log('free');
+                }
+            }
+
+            if(this.y + blockSize >= canvas.height){
+                this.y = this.y;
+                console.log("NEED NEW SHAPE");
+                cancelAnimationFrame(myAnimationRequest);
+
+                taken.x = this.x;
+                taken.y = this.y;
+                finishedShapes.push(taken);
+                console.log(finishedShapes);
+
+
+            }else if(this.y >= canvas.height && !free){
+                this.y = 0;
+            }
+            else{
+                this.y += blockSize;
+            }
+            this.draw();
+        }
+    };
+
+
+
+
+    var myAnimationRequest;
+
+    // SHAPES PLACEHOLDER ARRAY
+    var shapesArr = [];
+    var activeShape = [];
+    var finishedShapes = [];
+
+    shapesArr.push(new leftGun(100,0));
+
+
+
+    // shapesArr[0].update();
+
+
+    function animate(){
+        setTimeout(
+            function(){
+                myAnimationRequest = requestAnimationFrame(animate);
+            },
+        200);
+
+        // LOOP FOR UPDATING POSITION
+        for(var i = 0; i < shapesArr.length; i++){
+            shapesArr[i].update();
+        };
+    };
+
+    // animate();
+
 
 }
