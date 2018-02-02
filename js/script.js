9c3db6ee58f491d3e472e020e82fda4b4b3bb542
lkatkus <laimonas@katkus.eu>
2018-02-02 17:04:50 +0200

    Basic setup

--- js/script.js
+++ js/script.js
@@ -7,8 +7,16 @@ function init(){
     var block;
     var blockSize = 20;
 
+    // CONTROL VARIABLES
+    var direction;
+
+    // POSITIONING VARIABLES
+    var takenBelow;
+    var takenLeft;
+    var takenRight;
+
     // SETTING STARTING CANVAS SIZE
-    canvas.width = blockSize*12;
+    canvas.width = blockSize*24;
     canvas.height = blockSize*20;
 
     function drawBlock(){
@@ -19,172 +27,192 @@ function init(){
     }
     drawBlock();
 
-    var makeShape = {
-        leftGun: function(x,y){
-                console.log('leftGun');
-                this.x = x;
-                this.y = y;
-                ctx.putImageData(block,this.x,this.y);
-                ctx.putImageData(block,this.x+blockSize,this.y);
-                ctx.putImageData(block,this.x+blockSize*2,this.y);
-                ctx.putImageData(block,this.x+blockSize*2,this.y+blockSize);
-        },
-        rightGun: function(x,y){
-            console.log('rightGun');
-            this.x = x;
-            this.y = y;
-            ctx.putImageData(block,this.x,this.y);
-            ctx.putImageData(block,this.x+blockSize,this.y);
-            ctx.putImageData(block,this.x+blockSize*2,this.y);
-            ctx.putImageData(block,this.x,this.y+blockSize);
-        },
-        stick: function(x,y){
-            console.log('stick');
-            this.x = x;
-            this.y = y;
-            ctx.putImageData(block,this.x,this.y);
-            ctx.putImageData(block,this.x+blockSize,this.y);
-            ctx.putImageData(block,this.x+blockSize*2,this.y);
-            ctx.putImageData(block,this.x+blockSize*3,this.y);
-        },
-        box: function(x,y){
-            console.log('box');
-            this.x = x;
-            this.y = y;
-            ctx.putImageData(block,this.x,this.y);
-            ctx.putImageData(block,this.x+blockSize,this.y);
-            ctx.putImageData(block,this.x,this.y+blockSize);
-            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
-        },
-        pyramid: function(x,y){
-            console.log('pyramid');
-            this.x = x;
-            this.y = y;
-            ctx.putImageData(block,this.x,this.y);
-            ctx.putImageData(block,this.x+blockSize,this.y);
-            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
-            ctx.putImageData(block,this.x+blockSize*2,this.y);
-        },
-        rightKink: function(x,y){
-            console.log('rightKink');
-            this.x = x;
-            this.y = y;
-            ctx.putImageData(block,this.x,this.y+blockSize);
-            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
-            ctx.putImageData(block,this.x+blockSize,this.y);
-            ctx.putImageData(block,this.x+blockSize*2,this.y);
-        },
-        leftKink: function(x,y){
-            console.log('leftKink');
-            this.x = x;
-            this.y = y;
-            ctx.putImageData(block,this.x,this.y);
-            ctx.putImageData(block,this.x+blockSize,this.y);
-            ctx.putImageData(block,this.x+blockSize,this.y+blockSize);
-            ctx.putImageData(block,this.x+blockSize*2,this.y+blockSize);
-        },
-    };
+    // ADD CONTROLS
+    document.addEventListener('keydown',function(event){
+        if(event.key == 'ArrowRight'){
+            console.log('right');
+            direction = null;
+            direction = 'right';
+        }else if(event.key == 'ArrowLeft'){
+            console.log('left');
+            direction = null;
+            direction = 'left';
+        }else if(event.key == 'ArrowUp'){
+            console.log('rotate');
+
+        }else if(event.key == 'ArrowDown'){
+            console.log('down');
+        }else if(event.key == 0){
+            clearTimeout(myAnimationInterval);
+            cancelAnimationFrame(myAnimationRequest);
+        }
+    });
+
+
 
     function leftGun(x,y){
+
+        let moveBottom = true;
+        let moveLeft = true;
+        let moveRight = true;
+        direction = null;
+
         this.x = x;
         this.y = y;
 
-        this.draw = function (posX,posY){
-            // console.log('leftGun');
+        this.assemble = function(posX,posY){
+            if(shapeType == 1){
+                shape.push({x:posX, y:posY});
+                shape.push({x:posX + blockSize, y:posY});
+                shape.push({x:posX + blockSize * 2, y:posY});
+                shape.push({x:posX + blockSize * 2, y:posY + blockSize});
+            }else if(shapeType == 2){
+                shape.push({x:posX, y:posY});
+                shape.push({x:posX, y:posY + blockSize});
+                shape.push({x:posX, y:posY + blockSize * 2});
+                shape.push({x:posX - blockSize, y:posY + blockSize * 2});
+            }else if(shapeType == 3){
+                shape.push({x:posX, y:posY});
+                shape.push({x:posX, y:posY + blockSize});
+                shape.push({x:posX + blockSize, y:posY});
+                shape.push({x:posX + blockSize * 2, y:posY});
+            }else if(shapeType == 4){
+                shape.push({x:posX, y:posY});
+                shape.push({x:posX + blockSize, y:posY});
+                shape.push({x:posX, y:posY  + blockSize});
+                shape.push({x:posX, y:posY  + blockSize * 2});
+            }
+            this.draw();
+        };
 
-            // activeShape.push(ctx.putImageData(block,this.x,this.y);
-            ctx.putImageData(block,posX,posY);
-            // ctx.putImageData(block,this.x+blockSize,this.y);
-            // ctx.putImageData(block,this.x+blockSize*2,this.y);
-            // ctx.putImageData(block,this.x+blockSize*2,this.y+blockSize);
-        },
+        this.draw = function (posX,posY){
+            for(let i = 0; i < shape.length; i++){
+                ctx.putImageData(block,shape[i].x,shape[i].y);
+            };
+        };
 
         this.update = function(){
 
+            // CLEAN CANVAS BEFORE UPDATE
             void ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            let positionTaken;
-            let taken = {};
+            // CHECKING POSITION OF ACTIVE SHAPE
+            // CANVAS BOTTOM
+            for(let i = 0; i < shape.length; i++){
+                if(shape[i].y >= canvas.height - blockSize){
+                    moveBottom = false
+                    break;
+                }else{
+                    moveBottom = true;
+                };
+            };
 
+            // CHECK BOTTOM
+            if(moveBottom && finishedShapes.length > 0){
+                for(let i = 0; i < shape.length; i++){
+                    for(let x = 0; x < finishedShapes.length; x++){
+                        for(let j = 0; j < finishedShapes[x].length; j++){
+                            if(shape[i].y + blockSize == finishedShapes[x][j].y && shape[i].x == finishedShapes[x][j].x){
+                                moveBottom = false;
+                                break;
+                            };
+                        };
+                    };
+                };
+            };
 
-                // DRAW EXISTING SHAPES
-                for(let i = 0, j = finishedShapes.length; i < j; i++){
-                    this.draw(finishedShapes[i].x, finishedShapes[i].y);
+            // CHECK LEFT
+            if(moveBottom && finishedShapes.length > 0){
+                for(let i = 0; i < shape.length; i++){
+                    for(let x = 0; x < finishedShapes.length; x++){
+                        for(let j = 0; j < finishedShapes[x].length; j++){
+                            if(shape[i].x - blockSize == finishedShapes[x][j].x && shape[i].y == finishedShapes[x][j].y){
+                                moveLeft = false;
+                                break;
+                            };
+                        };
+                    };
+                };
+            };
+
+            // CHECK RIGHT
+            if(moveBottom && finishedShapes.length > 0){
+                for(let i = 0; i < shape.length; i++){
+                    for(let x = 0; x < finishedShapes.length; x++){
+                        for(let j = 0; j < finishedShapes[x].length; j++){
+                            if(shape[i].x + blockSize == finishedShapes[x][j].x && shape[i].y == finishedShapes[x][j].y){
+                                moveRight = false;
+                                break;
+                            };
+                        };
+                    };
+                };
+            };
+
+            // REDRAW FINISHED SHAPES
+            for(let i = 0; i < finishedShapes.length; i++){
+                for(let j = 0; j < finishedShapes[i].length; j++){
+                    ctx.putImageData(block,finishedShapes[i][j].x, finishedShapes[i][j].y);
+                }
+            }
+
+            if(moveBottom){
+                if(direction == 'left' && moveLeft){
+                    this.x -= blockSize;
+                }else if(direction == 'right' && moveRight){
+                    this.x += blockSize;
                 }
 
-                // CHECK IF CURRENT POSITION IS FREE
-                for(let i = 0, j = finishedShapes.length; i < j; i++){
-                    if(this.x == finishedShapes[i].x && this.y == finishedShapes[i].y - blockSize){
-                        console.log('TAKEN');
-                        positionTaken = true;
-                    }else{
-                        console.log('FREE');
-                        positionTaken = false;
-                    }
-                }
+                this.y += blockSize;
+            }else{
+                clearTimeout(myAnimationInterval);
+                cancelAnimationFrame(myAnimationRequest);
+                finishedShapes.push(shape);
+            }
 
-                // BOTTOM LINE
-                if(this.y + blockSize >= canvas.height){
-                    this.y = this.y;
+            // RESETING MOVEMENT FOR NEXT UPDATE
+            direction = null;
+            moveLeft = true;
+            moveRight = true;
 
-                    // ADD POSITION TO TAKEN OBJECT
-                    taken.x = this.x;
-                    taken.y = this.y;
-                    finishedShapes.push(taken);
-
-                    console.log('BOTTOM');
-                    clearTimeout(myAnimationInterval);
-                    cancelAnimationFrame(myAnimationRequest);
-
-                }else if(!positionTaken){
-                    this.y += blockSize;
-                    console.log('MOVING');
-
-                }else if(positionTaken){
-                    // ADD POSITION TO TAKEN OBJECT
-                    taken.x = this.x;
-                    taken.y = this.y;
-                    finishedShapes.push(taken);
-                    console.log("STOPED");
-                    clearTimeout(myAnimationInterval);
-                    cancelAnimationFrame(myAnimationRequest);
-                }
-
-            this.draw(this.x, this.y);
-        }
+            shape = [];
+            this.assemble(this.x,this.y);
+        };
     };
 
+    function createNewShape(){
+        activeShape = [];
+        activeShape.push(new leftGun(blockSize*5,0));
+        animate();
+    };
 
-document.addEventListener('click',function(){
-    console.log('shapesArr');
-    activeShape = [];
-    console.log(activeShape);
-    console.log('finishedShapes ' + finishedShapes);
-    activeShape.push(new leftGun(blockSize*5,0));
-    animate();
-})
+    // TESTING START
+    document.addEventListener('click',function(){
+        activeShape = [];
+        shapeType = Math.floor(Math.random()*4)+1;
+        activeShape.push(new leftGun(blockSize*5,0));
+        animate();
+    });
 
     var myAnimationRequest;
     var myAnimationInterval;
 
     // SHAPES PLACEHOLDER ARRAY
-    var shapesArr = [];
-    var activeShape = [];
+    var shape = [];
+    var shapeType = 1; /* FROM 1 TO 4 */
     var finishedShapes = [];
 
-
+    // MAIN ANIMATION FUNCTION
     function animate(){
-
         myAnimationInterval = setTimeout(
             function(){
                 myAnimationRequest = requestAnimationFrame(animate);
             },
-        200);
+        100);
 
         // LOOP FOR UPDATING POSITION
         for(var i = 0; i < activeShape.length; i++){
             activeShape[i].update();
         };
     };
-
-}
+};
